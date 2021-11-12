2898323f15e2faa980ad01f898bb1d67f7200a5f
Guowei <frankhome61@hotmail.com>
2021-11-12 00:18:50 -0800

    modified configs

--- create_dataset.py
+++ create_dataset.py
@@ -4,9 +4,9 @@ import shutil
 import numpy as np
 
 base_dir = "/home/guowei/Research/View-Synthesis-Current-Works/Neural-Scene-Flow-Fields/nerf_data/"
-scene_name = "scene006"
+scene_name = "scene003"
 
-output_name= "iccv-01"
+output_name= "iccv-03"
 
 output_dir = os.path.join(base_dir, output_name, "dense")
 output_image_dir = os.path.join(base_dir, output_name, "dense", "images")
@@ -25,7 +25,7 @@ for i, frame_idx in enumerate(range(frame_range[0], frame_range[1])):
 	cam_idx = cams[i % 2]
 	cam_img_path = os.path.join(base_dir, scene_name, "cam0{}".format(cam_idx), "*.jpg")
 	images = sorted(glob.glob(cam_img_path))[frame_range[0]:frame_range[1]]
-	shutil.copy2(images[i], os.path.join(output_image_dir, "{:05d}.jpg".format(frame_idx)))
+	shutil.copy2(images[i], os.path.join(output_image_dir, "{:05d}.jpg".format(i)))
 	new_poses.append(original_poses[cam_idx])
 
 new_poses = np.array(new_poses)
